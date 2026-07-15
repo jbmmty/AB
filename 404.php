534505bfb80598f3be930fc2c2b548cad3e79c91
jbmmty <h8zgw24gwt@privaterelay.appleid.com>
2026-07-15 08:31:50 -0600

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+ *
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ *
+ * @package Theratio
+ */
+?>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="page" class="site">
+<?php
+
+if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_location( 'single' ) ) {
+	theratio_404_builder();
+}
+
+?>
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
